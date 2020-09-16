86e250d47c233429abf79e77535da3f84cd57b12
Ubuntu <ignacio.sanchez@orsange.com>
2020-09-16 09:22:31 +0000

    commit del paso 3 alumno1

--- practicaGit/GitAux.java
+++ practicaGit/GitAux.java
@@ -1,10 +1,12 @@
 public class GitAux {
 	public void metodoAlumno1(){
 		System.out.println("metodo 1");
+		System.out.println("modificado por alumno1"); //insertado por alumno 1
 	}
 	public vold metodoComunitario(){
 		// comentario para pruebas
 		System.out.println("Aquí escribimos todos");
+		System.out.println("alumno1 inserta esta sentencia");
 	}
 }
 
