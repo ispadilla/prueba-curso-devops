1f227b2ff76ef78dfd2d9a0dceda510921040303
Ubuntu <ignacio.sanchez@orsange.com>
2020-09-16 09:18:42 +0000

    commit inicial ejecrcicio 1

---
+++ practicaGit/GitAux.java
@@ -0,0 +1,12 @@
+public class GitAux {
+	public void metodoAlumno1(){
+		System.out.println("metodo 1");
+	}
+	public vold metodoComunitario(){
+		// comentario para pruebas
+		System.out.println("Aquí escribimos todos");
+	}
+}
+
+
+
